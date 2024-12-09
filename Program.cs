7a41aca83a91264370aff4e71fff4bd012b48e60
Rezamrs22 <rezamrs22@gmail.com>
2024-12-09 13:04:11 +0100

    sista uppdatering

--- Program.cs
+++ Program.cs
@@ -1,13 +1,101 @@
-﻿using Bibliotek.Models;
-using Bibliotekssystem.Models;
+﻿using System;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Microsoft.Identity.Client.Extensibility;
+using Bibliotek.Models;
+using Bibliotek.CRUD;
 
 class Program
 {
     static void Main(string[] args)
     {
-       
-    
+        int menuSel;
+        do
+        {
+            //Console.Clear();
+            menuSel = MenuSelection();
+            MenuExecution(menuSel);
+
+        } while (menuSel != 5);
+
+        Console.ForegroundColor = ConsoleColor.DarkYellow;
+        System.Console.WriteLine("\nThank you for your time");
+        Console.ForegroundColor = ConsoleColor.White;
+        
     }
+
+    private static void MenuExecution(int menuSel)
+    {
+        switch (menuSel)
+        {
+            case 1:
+                CreateBookOrAuthor.Run();
+                break;
+            case 2:
+                DeleteBookeOrAuthors.Run();
+                break;
+            case 3:
+                ReadBooksOrAuthors.Run();
+                break;
+            case 4:
+                UpdateBooksOrAuthors.Run();
+                break;
+            case 5:
+                System.Console.WriteLine("Exiting program...");
+                break;
+            default:
+                Console.ForegroundColor = ConsoleColor.Red;
+                System.Console.WriteLine("Invalid selections. Try again.");
+                Console.BackgroundColor = ConsoleColor.White;
+                break;
+
+        }
+    }
+
+    private static int MenuSelection()
+    {
+        while (true)
+        {
+            try
+            {
+                
+                System.Console.WriteLine("Welcome to REZA SAFDARI's library");
+                System.Console.WriteLine("1. Create a new author od book");
+                System.Console.WriteLine("2. Remove author or books");
+                System.Console.WriteLine("3. List Books With Authors");
+                System.Console.WriteLine("4. Add a loan for a book");
+                System.Console.WriteLine("5. Quit");
+                   
+                   
+                int menuSel = int.Parse(Console.ReadLine());
+                if (menuSel >= 1 && menuSel <= 5)
+                {
+                    return menuSel;
+                }
+                    
+                    
+                Console.ForegroundColor = ConsoleColor.Red;
+                System.Console.WriteLine("Invalid selection");
+                Console.ForegroundColor = ConsoleColor.White;
+                    
+                
+            }
+            catch(FormatException)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Invalid input. Please enter a number.");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Error: {ex.Message}");
+                Console.ForegroundColor = ConsoleColor.White;
+                continue;
+            }
+            
+        }
+
+    }
+
+    
 }
--- obj/Debug/net8.0/Bibliotek.AssemblyInfo.cs
+++ obj/Debug/net8.0/Bibliotek.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bibliotek")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5be9ae1cd1861a5690b9e501f88028bb645fdd45")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c28a0f134fe2821653b1d78f3e98a04e6451e3d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bibliotek")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bibliotek")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
